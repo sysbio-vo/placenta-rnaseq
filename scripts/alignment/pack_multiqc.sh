14c720ce3c5ea491fe9b4852882604313bac8642
Stanislav Zubenko <dantistnfs@gmail.com>
2021-03-18 16:10:08 +0200

    pack multiqc update

--- scripts/alignment/pack_multiqc.sh
+++ scripts/alignment/pack_multiqc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source activate snakemake-rnaseq
 
-multiqc fastp_reports/ aligned_star/ --title "Preeclampsia placenta RNA-seq report" -b ""
+multiqc fastp_reports/ aligned_star/ --title "Preeclampsia placenta RNA-seq report" -b "" -v
 
-tar -czf multiqc_report.tar.gz Preeclampsia_placenta_RNA-seq_report_multiqc_report.html \ 
-             Preeclampsia_placenta_RNA-seq_report_multiqc_report/
+tar -czf multiqc_report.tar.gz Preeclampsia-placenta-RNA-seq-report_multiqc_report.html Preeclampsia-placenta-RNA-seq-report_multiqc_report_data/
+
