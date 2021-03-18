efbae52a03dae4c5949a5b2dac5a314a5b66bb85
Stanislav Zubenko <dantistnfs@gmail.com>
2021-03-18 16:11:17 +0200

    automatic renaming

--- scripts/alignment/pack_multiqc.sh
+++ scripts/alignment/pack_multiqc.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 source activate snakemake-rnaseq
 
+cd fastp_reports
+
+find . -name '*_fastp_report.json' -exec bash -c 'cp $0 ${0/_report/}' {} \;
+
+cd ..
+
 multiqc fastp_reports/ aligned_star/ --title "Preeclampsia placenta RNA-seq report" -b "" -v
 
 tar -czf multiqc_report.tar.gz Preeclampsia-placenta-RNA-seq-report_multiqc_report.html Preeclampsia-placenta-RNA-seq-report_multiqc_report_data/
